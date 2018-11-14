bce715dbbd77c61655be15e8674a0744d98d3305
yves <magneticchen@gmail.com>
2018-11-14 23:07:57 +0800

    bug fixes

--- src/browser/shell.js
+++ src/browser/shell.js
@@ -1,5 +1,5 @@
 let _NSc = new NSc();
-_NSc.connect('www.nooxy.tk', '1487', 'admin');
+_NSc.connect('0.0.0.0', '1487');
 _NSc.createActivitySocket('NoShell', (err, as)=>{
   console.log('adfas');
   $(function () {
